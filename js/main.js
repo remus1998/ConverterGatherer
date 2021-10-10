c4c7afadc0f29afad4f446d8ddd7048b3051b94f
Remus <mprekuta@gmail.com>
2021-10-10 20:08:39 +0200

    prototype page created

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function onload(){
+
+  var swiper = new Swiper(".mySwiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+} window.addEventListener('load',onload)
